18a8aecde1ff2164d5e4da0bd4c328ee771f664c
Kevin Lim <zzkevinlim@gmail.com>
2022-04-04 14:27:02 +1200

    configured tailwind, installed tailwind plugins, installed statamic plugins, installed debugbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,10 @@ module.exports = {
     },
 
     plugins: [
+        require("@tailwindcss/aspect-ratio"),
         require("@tailwindcss/forms"),
+        require("@tailwindcss/line-clamp"),
         require("@tailwindcss/typography"),
+        require("tailwindcss-debug-screens"),
     ],
 };

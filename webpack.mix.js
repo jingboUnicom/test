a44ea606942af86da95793afde258f5f1988c0e5
Kevin Lim <zzkevinlim@gmail.com>
2022-04-04 14:10:46 +1200

    configured tailwind and alpinejs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,12 +11,25 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
+mix.options({
+    processCssUrls: false,
+    postCss: [require("tailwindcss"), require("autoprefixer")],
+});
+
+mix.setPublicPath('public');
+
+// mix.js("resources/js/app.js", "public/js").postCss(
+//     "resources/css/app.css",
+//     "public/css",
+//     [require("postcss-import"), require("tailwindcss")]
+// );
+
+mix.sass('resources/sass/app.scss', 'css');
+
+mix.js('resources/js/app.js', 'js');
 
 if (mix.inProduction()) {
     mix.version();
 }
+
+mix.disableSuccessNotifications();
